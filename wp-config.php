29551075e0650abb86f9e0586edb596c1abd45b8
Oleksandr Nadtoka <alex.nadtoka@gmail.com>
2017-02-23 16:53:50 +0200

    wp conf adjusted with right db details

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'wp_fitfam');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wp_fitfam');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '4623mj2X8Rr48iN');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
